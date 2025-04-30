306e08d3773b0230a22adaf513e512182f814a5b
Jaehyeok-Soh <revol3154@gmail.com>
2025-05-01 00:27:38 +0900

    edit

--- 250429/250429.cpp
+++ 250429/250429.cpp
@@ -3,10 +3,13 @@
 int main()
 {
 	cMyString cStr;
+
 	cStr = "hello";
 	std::cout << cStr << std::endl;
+
 	cStr = cStr + " world";
 	std::cout << cStr << std::endl;
+
 	if (cStr == "hello world")
 	{
 		std::cout << "true";
